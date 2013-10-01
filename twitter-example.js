aed84cd23779a576ea7ba83b8da13577af1f3d08
Justin Martin <justin.martin@corda.com>
2013-10-01 08:58:04 -0600

    Put alerts in bookmarklet to verify you are logged in etc.

--- twitter-example.js
+++ twitter-example.js
@@ -1,3 +1,13 @@
+if (window.location.href.indexOf('twitter.com') === -1) {
+  alert('Go to http://twitter.com, then click the bookmarklet.');
+  throw new Error();
+}
+
+if ($('.account-summary.account-summary-small').length < 1) {
+  alert('Log in to twitter.com, then click the bookmarklet.');
+  throw new Error();
+}
+
 var tweetText = 'Check it out! I just tweeted using #BrowserStig. \
 Developer Friendly browser automation! \
 http://justspamjustin.github.io/browserstig #browserautomation';
